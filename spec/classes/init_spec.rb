c5a0d5f361ca78413ca39d9e649f5c05c902bbdd
thefungfupie <thefungfupie@gmail.com>
2014-07-14 18:21:38 -0500

    Demo

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+describe 'unika' do
+  context 'on CentOS' do
+    let(:facts) { {
+      :operatingsystem => 'CentOS'
+    } }
+    it { 
+      should contain_class('unika') 
+      should contain_file('/etc/unika-version').with(
+        'owner' => 'root',
+        'group' => 'root',
+        'mode'  => '0600'
+      ).with_content("unika for #{facts[:operatingsystem]}\n")
+    }
+  end
+  context 'on RedHat' do
+    let(:facts) { {
+      :operatingsystem => 'RedHat'
+    } }
+    it { 
+      should contain_class('unika') 
+      should contain_file('/etc/unika-version').with(
+        'owner' => 'root',
+        'group' => 'root',
+        'mode'  => '0600'
+      ).with_content("unika for #{facts[:operatingsystem]}\n")
+    }
+  end
+  context 'on OS X' do
+    let(:facts) { {
+      :operatingsystem => 'OS X'
+    } }
+    it { 
+      should contain_class('unika') 
+      should contain_file('/etc/unika-version').with(
+        'owner' => 'root',
+        'group' => 'wheel',
+        'mode'  => '0600'
+      ).with_content("unika for #{facts[:operatingsystem]}\n")
+    }
+  end
+  context 'on Debian' do
+    let(:facts) { {
+      :operatingsystem => 'Debian'
+    } }
+    it { 
+      should contain_class('unika') 
+      should contain_file('/etc/unika-version').with(
+        'owner' => 'root',
+        'group' => 'root',
+        'mode'  => '0600'
+      ).with_content("unika for #{facts[:operatingsystem]}\n")
+    }
+  end
+end
